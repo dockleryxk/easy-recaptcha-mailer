eb4f86c0466ed5168bb7f86e5ab42561493aa913
Rich <rajeffords@gmail.com>
2017-02-16 15:35:25 -0500

    first commit, had to delete the old repository because I kept my recaptcha secret in the code

---
+++ README.md
@@ -0,0 +1,19 @@
+# Easy Recaptcha Mailer
+
+>A no frills php script that will send email from a server. Comes with server-side recaptcha validation and sanitation.
+
+See it in action on my website, [richardjeffords.com](https://richardjeffords.com)
+
+## How
+
+- Put it on your server
+- Open it up
+- Change the variables at the top right under the if statement
+- Call it with a POST request (check out the js example)
+- Do whatever you want
+
+## License [![alt text](http://www.wtfpl.net/wp-content/uploads/2012/12/wtfpl-badge-4.png)](http://www.wtfpl.net/)
+
+This script is made available under a [WTFPL license](http://www.wtfpl.net/)
+
+Copyright © 2017 Richard Jeffords
---
+++ easy-recaptcha-mailer.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * @author Richard Jeffords <rajeffords@gmail.com>
+ *
+ * It's simple -- verify the recaptcha server-side (here we are)
+ * and then send an email of the contact form contents
+ *
+ * The POST request needs five fields:
+ ****** name
+ ***** email
+ **** message
+ *** subject
+ ** g-recaptcha-response
+ */
+
+// Only process POST reqeusts.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    /*****************************************
+     ****** Add your information here ********
+     *****************************************/
+      $recaptchaSecret = '';
+      $email_address = '';
+     /****************************************
+      ****************************************/
+
+    header('Access-Control-Allow-Origin: *');
+    $url = 'https://www.google.com/recaptcha/api/siteverify';
+    $data = array(
+            'secret' => $recaptchaSecret,
+            'response' => $_POST["g-recaptcha-response"],
+            'remoteip' => $_SERVER["REMOTE_ADDR"]
+            );
+
+    // use key 'http' even if you send the request to https://...
+    $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+                )
+            );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    //PHP sees JSON as a string
+    $result = get_object_vars(json_decode($result));
+
+    //if the API call fails or the key is bad
+    if ($result === FALSE || !$result['success']) { /* Handle error */
+        http_response_code(500);
+        echo 'Recaptcha server error, try again later or email me directly at '. $email_address;
+        exit;
+    }
+
+    // Get the form fields and remove whitespace.
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $subject = strip_tags(trim($_POST["subject"]));
+    $subject = str_replace(array("\r","\n"),array(" "," "),$subject);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
+
+    // Check that data was sent to the mailer.
+    if (empty($name) || empty($subject) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Set a 400 (bad request) response code and exit.
+        http_response_code(400);
+        echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+        exit;
+    }
+
+    // Set the recipient email address.
+    $recipient = $email_address;
+
+    // Build the email content.
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers.
+    $email_headers = "From: Easy Recaptcha Mailers <erm@example.com>";
+
+    // Send the email.
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        // Set a 200 (okay) response code.
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    }
+    else {
+        // Set a 500 (internal server error) response code.
+        http_response_code(500);
+        echo "Oops! Something went wrong with the mail server and I couldn't send your message. Try again later or email me directly at " . $email_address;
+    }
+
+}
+else {
+    // Not a POST request, set a 403 (forbidden) response code.
+    http_response_code(403);
+    echo "There is either something terribly wrong, or you are up to no good.";
+}
